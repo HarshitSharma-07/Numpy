5af87c57c53c909d618e3d3c8c801cd3e4606ac1
HarshitSharma-07 <rawatsarika76@gmail.com>
2026-07-05 18:02:37 +0530

    update slicing and indexing file

--- 5 slicing and indexing.py
+++ 5 slicing and indexing.py
@@ -13,6 +13,13 @@ print(arr[2:7:2])
 print(arr[::-1])
 
 
+# how to print complete rows and columns in a 2d array
+
+print("sliced row: ",arr_2d[2])
+print("sliced column: ",arr_2d[:,2])  #[:,x] ":" means that i want to print all the rows 
+
+
+
 # fancy indexing
 # getting multiple elements at once
 
